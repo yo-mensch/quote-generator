27e06d7e53dcc600b714fade47551e9103086695
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2020-09-19 11:09:06 +0300

    added random quote api

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 //console.log("Hello world");
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //get quote from api
 async function getQuote(){
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try{
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+
+        if(data.quoteAuthor===''){
+            authorText.innerText = "Unknown";
+        } else{
+            authorText.innerText = data.quoteAuthor;
+        }
+        quoteText.innerText = data.quoteText;
+
+        if(data.quoteText.length>120){
+            quoteText.classList.add("long-quote");
+        } else{
+            quoteText.classList.remove("long-quote");
+        }
+        //console.log(data);
     } catch(error){
-        console.log("Oopsie doopsie, no quote ", error);
+        getQuote();
+        //console.log("Oopsie doopsie, no quote ", error);
     }
 }
 
