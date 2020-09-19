48e6edf8019d81cecfbdfd782768217c4de019c0
Rasa Jurgutytė <53710574+yo-mensch@users.noreply.github.com>
2020-09-19 10:47:31 +0300

    styling

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //console.log("Hello world");
+//get quote from api
+async function getQuote(){
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error){
+        console.log("Oopsie doopsie, no quote ", error);
+    }
+}
+
+// On load
+getQuote();
